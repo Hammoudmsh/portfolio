9a9dd597d9049352d1cce7a1b0a4b47a513d7776
Mohammed Hammoud <Mohammedhamoudmsh594@gmail.com>
2024-01-16 18:20:21 +0300

    Add files via upload

---
+++ common.py
@@ -0,0 +1,59 @@
+RESUME_LINK = ""
+
+
+ABOUT_ME_TEXT_HEADER = "About Me"
+ABOUT_ME_TEXT_SUB_HEAD = "Researcher in internet of Things of Medical Applications"
+ABOUT_ME_TEXT = f"""
+	👋🏻 Hi, I'm  ! I'm a data science and analytics undergraduate based in Singapore. Having prior relevant experiences in tech, reinsurance and consulting sectors, I am constantly seeking unique internships to broaden my horizons before embarking on my data career upon graduation.\n
+	💼 With the COVID-19 pandemic behind us, I believe there is potential for data science to be applied in the retail industry. In response to the increasing demand for data analytics from both online and brick-and-mortar sales, I am thus aiming to enter this industry for my first full-time job.\n
+	🏋🏻 In addition, I like to exercise in the gym, run, write, play video games and... enjoy eating good food in my free time!\n
+	👨🏼‍💻 Academic interests: Data Visualization, Market Basket Analysis, Recommendation Systems, Natural Language Processing\n
+	💭 Ideal Career Prospects: Data Analyst, Data Scientist, Data Engineer, Business Intelligence Analyst, Product Manager\n
+"""
+
+ABOUT_ME_TEXT = f"""
+I am a highly motivated specialist in the field of Internet of Things (IoT) applications. My professional experience includes working with microcontrollers, sensors, and real-time operating systems (RTOS), with a comprehensive understanding of embedded software design to support IoT initiative applications, especially in medical applications. Furthermore, I possess a strong research background  to combine different technologies in one product, such as computer vision, Ml, and deep learning.
+"""
+
+ABOUT_ME_RESUME = f"📄 [Resume]({RESUME_LINK})"
+
+
+page_title = "Mohammed Hammoud Portfolio"
+page_icon = "desktop_computer"
+PERSON_NAME = "Mohammed Hammoud"
+youtube_url = ""
+linkedin_url = "https://www.linkedin.com/in/mohammed-h-b28987239/en?trk=contact-info"
+wordpress_url = ""
+
+
+email_url = "Mohammed.Hammoud@skoltech.ru"
+github_url = "https://github.com/Hammoudmsh"
+
+GoogleScholar_url = "https://scholar.google.com/citations?user=Hrj7by4AAAAJ&hl=ar"
+ResearchGate_url = "https://www.researchgate.net/profile/Mohammed-Hammoud-2"
+ORCID_url = "https://orcid.org/0000-0002-7967-9057"
+
+
+
+
+
+ICONS_IMAGES = {
+                "ABOUT_ME_IMAGE":"images/person_photo.png",
+                "youtube": "https://img.icons8.com/ios-filled/100/ff8c00/youtube-play.png",
+                "linkedin": "https://img.icons8.com/fluency/48/linkedin.png",
+                "github": "https://img.icons8.com/metro/26/github.png",
+                "wordpress": "https://icons8.com/icon/5eocsnCvaiRd/bank-card-dollar.png",
+                "email": "https://img.icons8.com/ios-filled/50/new-post.png",
+                "researchgate": "https://img.icons8.com/external-aficons-studio-flat-aficons-studio/100/external-research-online-learning-aficons-studio-flat-aficons-studio.png",
+                # https://img.icons8.com/external-tal-revivo-fresh-tal-revivo/28/external-researchgate-a-social-networking-site-for-scientists-and-researchers-to-share-papers-logo-fresh-tal-revivo.png
+                "googlescholar": "https://img.icons8.com/ios/100/google-scholar--v2.png",
+                "orcid": "https://img.icons8.com/fluency/48/000000/student-center.png",
+                "paper":"https://img.icons8.com/ios-filled/100/document-1.png",
+                "certificate":"https://img.icons8.com/ios/100/000000/diploma.png",#"https://img.icons8.com/fluency/100/certificate--v2.png",
+                "download":"https://img.icons8.com/officel/100/download--v1.png",
+                "conference":"https://img.icons8.com/color/100/politician.png",
+                "languages":"https://img.icons8.com/external-smashingstocks-mixed-smashing-stocks/100/external-Languages-language-and-translation-smashingstocks-mixed-smashing-stocks.png",
+                
+            }
+            # <img src="https://img.icons8.com/color/2x/search">
+
